2c545eeb55a6c5825cc41e2aa886fe2697e0128a
Vyshnavi <pasupuletivyshnavi98@gmail.com>
2026-07-07 15:21:57 +0530

    Fixed authentication and updated UI

--- store/urls.py
+++ store/urls.py
@@ -8,11 +8,7 @@ urlpatterns = [
 
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path(
-        'logout/',
-        auth_views.LogoutView.as_view(next_page='home'),
-        name='logout'
-    ),
+    path('logout/',auth_views.LogoutView.as_view(),name='logout' ),
 
     path('profile/', views.profile, name='profile'),
 
